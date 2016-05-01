2d9bb20d7cb6f161c7fd9234a57c981b7b4002d9
ngatmaitan <ngatmaitan@smu.edu>
2016-04-30 23:08:39 -0500

    updated stop words list

--- crawler.py
+++ crawler.py
@@ -178,7 +178,7 @@ class Crawler:
         This method stores the list of stopwords from a file to the class
         variable list self.stopwords.
         """
-        self.stopwords = [line.rstrip('\n') for line in open(file)]
+        self.stopwords = [line.rstrip(' \n').lower() for line in open(file)]
 
     def prepare_text(self, text) :
         """
@@ -222,7 +222,6 @@ class Crawler:
         1 + log(number of times word appears in a document) * log(total documents/ how many documents the word appears in)
         """
         for i in self.all_words[word] :
-            #return i[1] * log10(len(visited)/self.all_words_freq[word][0])
             return (1 + log10(i[1])) * log10(len(visited)/self.all_words_freq[word][0])
 
     def write_output(self, visited, external, jpeg, broken, dictionary) :
